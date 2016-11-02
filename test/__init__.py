0acdceee820ea2bb9ed4c5ae06095ab5f2fe0846
Todd Bodnar <todd.bodnar@breinify.com>
2016-11-02 10:13:27 -0700

    new python version of api

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,28 @@
-##TODO: tests!
+import unittest
+import breinify
+
+breinify.setup("abcd", "5e9xqoesiygkuzddxjlkaq==")
+
+
+class TestHashFunctions(unittest.TestCase):
+    def test_sign_activity(self):
+        toPush = {"unixTimeStamp": 1451962516, "activity": {"type": "search"}}
+        breinify.__signActivity(toPush)
+        self.assertEqual("rsXU0ozhfzieNLA2jQs2h2e4sz2+qHGxbgSYyfWr5EM=", toPush["signature"])
+
+    def test_sign_lookup(self):
+        toPush = {"unixTimeStamp": 1451962516, "lookup": {
+            "dimensions": [
+                "firstname",
+                "gender",
+                "age",
+                "agegroup",
+                "digitalfootprint",
+                "images"
+            ]
+        }}
+        breinify.__signLookup(toPush)
+        self.assertEqual("o/LBuRFrlbZDG9OLdZRrYC2ERYqwIxdv4Dloj/GCeLQ=", toPush["signature"])
+
+if __name__ == '__main__':
+    unittest.main()
